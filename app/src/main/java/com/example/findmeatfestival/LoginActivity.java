de7d08918cbc89479e63d42c1c5771c3165d0608
Michals1988 <michal.seifert@wp.pl>
2021-03-31 00:21:46 +0200

    add LoginActivity (working buttons) add RegisterActivity

---
+++ app/src/main/java/com/example/findmeatfestival/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.example.findmeatfestival;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button buttonLogin=findViewById(R.id.buttonLogin);
+        Button buttonGoToRegister=findViewById(R.id.buttonGoToRegister);
+
+
+
+        buttonLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){}
+        });
+
+        buttonGoToRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Context context = v.getContext();
+                Intent intent = new Intent(context, RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/example/findmeatfestival/MainActivity.java
+++ app/src/main/java/com/example/findmeatfestival/MainActivity.java
@@ -2,14 +2,30 @@ package com.example.findmeatfestival;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println("Hello World");
+
         setContentView(R.layout.activity_main);
+
+        Button buttonTest=findViewById(R.id.button);
+
+        buttonTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context=v.getContext();
+
+                Intent testIntent=new Intent(context, LoginActivity.class);
+                startActivity(testIntent);
+            }
+        });
     }
 }
