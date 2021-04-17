6c1652fb445124902621a9b4503008ce20d43b14
Michals1988 <michal.seifert@wp.pl>
2021-04-17 23:15:55 +0200

    -add new site : Festival, Friends -respons menu

--- app/src/main/java/com/example/findmeatfestival/RegisterActivity.java
+++ app/src/main/java/com/example/findmeatfestival/RegisterActivity.java
@@ -2,7 +2,11 @@ package com.example.findmeatfestival;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button buttonLogin=findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context=v.getContext();
+                Intent intentBackToLogin=new Intent(context, LoginActivity.class);
+                startActivity(intentBackToLogin);
+            }
+        });
     }
 }
